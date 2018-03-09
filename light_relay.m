e04188df9007ae2020913939a2cc0fc8bf10f27e
Peter Kazarinoff <peter.kazarinoff@pcc.edu>
2018-03-08 17:02:53 -0800

    Update light_relay.m

--- light_relay.m
+++ light_relay.m
@@ -1,25 +1,25 @@
-%% Arduino Group Project: Write Serial to an Arduino
+%% Arduino Group Project: Use MATLAB to turn on and off a relay connected to an Arduino
 % This code will connect to an Arduino over a Serial Port
 % It will ask the user for an H or an L or a q
-% H is sent to Arduino over serial
-% L is sent to Arduino over serial
+% H is sent to Arduino over serial (which turns Arduino LED on)
+% L is sent to Arduino over serial (which turns Arduino LED off)
 % q quits the program
 
 %%
 clc, clear, close all
 
 %% Set up serial port and ensure it outputs data
-% Change the Port the Arduino is connected to
+% !!! Check the Port (COM#) the Arduino is connected to using Windows Device Manager !!!
 Port = 'COM4';         % Port the Arduino is connected to
-delete(instrfindall);  %deletes any connected ports
-a = serial(Port); % connect to the arduino in order to read information 
+delete(instrfindall);  % deletes any connected ports
+a = serial(Port); % connect to the Arduino in order to read information 
 fopen(a);         % opens the serial port
 pause(1);         % pause() for 1 second to make sure a connection is made
 out = instrfind('Port',Port); % see if the port you specified is open
 disp('Serial Port is Open')
 
 %% Ask User for Input and send input over serial
-usr_input = 'm';% initialize the usr_input variable
+usr_input = 'm';   % initialize the usr_input variable, so that we enter into the while loop
 time_matrix = [];
 tracker = [];
 timeON = 0;
@@ -36,12 +36,12 @@ if strcmp(usr_input,'L')            % if L is typed
     fprintf(a,'%s',usr_input); %this will send H to theaArduino
     timeOFF = clock;
     tracker = timeOFF - timeON;
-    % Need a Way to track amount of time light is on and off
+
+% Need a Way to track amount of time light is on and off
 %Use clock function within Loop to track when the light is on or off
 %as of right now this will only track 1 on/off pair
 
-%important to remember that the clock func prints a 1 by 6 vector of the
-%form year,month,day,hour,min,sec using a 24 hour clock
+    %important to remember that the clock func prints a 1 by 6 vector of the
     %need a scalar value in hours to fill time matrix
     timeHRS = 8760*tracker(1)+720*tracker(2)+24*tracker(3)+tracker(4)+(1/60)*tracker(5)+(1/3600)*tracker(6);
     time_matrix(end+1) = timeHRS;
